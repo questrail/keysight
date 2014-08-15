e3ffe2e1a423b3ad6db7a101033bc1f0dc1f4b66
Matthew Rankin <matthew@questrail.com>
2014-08-15 14:15:20 -0500

    Tests pass with a single CSV example file.

--- keysight/n9340.py
+++ keysight/n9340.py
@@ -14,7 +14,6 @@ from __future__ import absolute_import
 
 # Standard module imports
 import csv
-import os.path
 
 # Data analysis related imports
 import numpy as np
@@ -23,11 +22,55 @@ import numpy as np
 def read_csv_file(filename):
     """Read csv file into a numpy array
     """
-    # FIXME: Test a file with blank lines in the CSV file.
-    header_data = {}
+    n9340_header = {}
     with open(filename, 'rb') as csvfile:
         data = csv.reader((line.replace(b'\0', b'') for line in csvfile),
                           delimiter=b',')
-        header_data['file'] = data.next()[1]
+        n9340_header['file'] = data.next()[1]
+        n9340_header['system_parameter'] = data.next()[1]
+        temp_row = data.next()
+        n9340_header['ref'] = float(temp_row[1])
+        n9340_header['ref_offset'] = float(temp_row[3])
+        temp_row = data.next()
+        n9340_header['start_freq'] = float(temp_row[1])
+        n9340_header['stop_freq'] = float(temp_row[3])
+        temp_row = data.next()
+        n9340_header['center_freq'] = float(temp_row[1])
+        n9340_header['span_freq'] = float(temp_row[3])
+        temp_row = data.next()
+        n9340_header['vbw'] = float(temp_row[1])
+        n9340_header['vbw_mode'] = temp_row[2]
+        n9340_header['rbw'] = float(temp_row[4])
+        n9340_header['rbw_mode'] = temp_row[5]
+        temp_row = data.next()
+        n9340_header['vbw_to_rbw'] = float(temp_row[1])
+        n9340_header['vbw_to_rbw_mode'] = temp_row[2]
+        n9340_header['sweep_time'] = float(temp_row[4])
+        n9340_header['sweep_time_mode'] = temp_row[5]
+        temp_row = data.next()
+        n9340_header['attenuation'] = float(temp_row[1])
+        n9340_header['attenuation_mode'] = temp_row[2]
+        temp_row = data.next()
+        n9340_header['scale_div'] = float(temp_row[1])
+        n9340_header['scale_type'] = temp_row[3]
+        n9340_header['preamp'] = temp_row[5]
+        temp_row = data.next()
+        n9340_header['psd_mode'] = temp_row[1]
+        temp_row = data.next()
+        temp_row = data.next()
+        # FIXME(mdr) Use a regex or something better than this
+        n9340_header['trace_unit'] = temp_row[0].split(':')[2][:-1]
+        temp_row = data.next()
+        n9340_header['frequency'] = temp_row[0]
 
-    print(header_data['file'])
+        n9340_array = []
+
+        for row in data:
+            n9340_array.append((float(row[0]), float(row[1])))
+
+        n9340_data = np.array(
+            n9340_array,
+            dtype={'names': ('frequency', 'amplitude_db'),
+                   'formats': ('f8', 'f8')})
+
+    return (n9340_header, n9340_data)
--- tests/test_n9340.py
+++ tests/test_n9340.py
@@ -24,12 +24,47 @@ class TestReadingCSVFiles(unittest.TestCase):
 
     def setUp(self):
         test_csv_file = Path(TEST_DIR, 'sample_data', 'RET1AMB.CSV')
+        print(test_csv_file)
         (self.n9340_header, self.n9340_data) = n9340.read_csv_file(
             test_csv_file)
 
-    def test_reading_n9340_file(self):
-        self.assertEual(self.n9340_header['file'],
-                        '/bd0/N9340DATA/RET1AMB.CSV')
+    def test_header_when_reading_n9340_file(self):
+        self.assertEqual(self.n9340_header['file'],
+                         '/bd0/N9340DATA/RET1AMB.CSV')
+        self.assertEqual(self.n9340_header['system_parameter'],
+                         'Default Unit:dBm/Hz/s')
+        self.assertEqual(self.n9340_header['ref'], 0.0)
+        self.assertEqual(self.n9340_header['ref_offset'], 0.0)
+        self.assertEqual(self.n9340_header['start_freq'], 100000)
+        self.assertEqual(self.n9340_header['stop_freq'], 30000000)
+        self.assertEqual(self.n9340_header['center_freq'], 15050000)
+        self.assertEqual(self.n9340_header['span_freq'], 29900000)
+        self.assertEqual(self.n9340_header['vbw'], 10000)
+        self.assertEqual(self.n9340_header['vbw_mode'], 'AUTO')
+        self.assertEqual(self.n9340_header['rbw'], 10000)
+        self.assertEqual(self.n9340_header['rbw_mode'], 'MAN')
+        self.assertEqual(self.n9340_header['vbw_to_rbw'], 1)
+        self.assertEqual(self.n9340_header['vbw_to_rbw_mode'], 'AUTO')
+        self.assertEqual(self.n9340_header['sweep_time'], 1.278503)
+        self.assertEqual(self.n9340_header['sweep_time_mode'], 'AUTO')
+        self.assertEqual(self.n9340_header['attenuation'], 20)
+        self.assertEqual(self.n9340_header['attenuation_mode'], 'AUTO')
+        self.assertEqual(self.n9340_header['scale_div'], 10)
+        self.assertEqual(self.n9340_header['scale_type'], 'LOG')
+        self.assertEqual(self.n9340_header['preamp'], 'OFF')
+        self.assertEqual(self.n9340_header['psd_mode'], 'OFF')
+        self.assertEqual(self.n9340_header['trace_unit'], 'dBm')
+
+    def test_data_when_reading_n9340_file(self):
+        self.assertEqual(self.n9340_data.shape, (461,))
+        self.assertEqual(self.n9340_data['frequency'][0], 100000)
+        self.assertEqual(self.n9340_data['amplitude_db'][0], -47.25)
+        self.assertEqual(self.n9340_data['frequency'][1], 165000)
+        self.assertEqual(self.n9340_data['amplitude_db'][1], -47.11)
+        self.assertEqual(self.n9340_data['frequency'][-1], 30000000)
+        self.assertEqual(self.n9340_data['amplitude_db'][-1], -74.7)
+        self.assertEqual(self.n9340_data['frequency'][-2], 29935000)
+        self.assertEqual(self.n9340_data['amplitude_db'][-2], -75.66)
 
 if __name__ == '__main__':
     unittest.main()
