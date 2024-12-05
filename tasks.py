1c0e4c1cd4e511956d3149caeaba2ea6a69cddb5
Matthew Rankin <matthew@questrail.com>
2024-12-05 08:34:07 -0600

    Use hatch instead of setuptools to build/deploy

--- tasks.py
+++ tasks.py
@@ -1,10 +1,9 @@
 # -*- coding: utf-8 -*-
-# Copyright (c) 2013-2023 The keysight developers. All rights reserved.
+# Copyright (c) 2013-2024 The keysight developers. All rights reserved.
 # Project site: https://github.com/questrail/keysight
 # Use of this source code is governed by a MIT-style license that
 # can be found in the LICENSE.txt file for the project.
-"""Invoke based tasks for keysight
-"""
+"""Invoke based tasks for keysight"""
 
 from invoke import run, task
 from unipath import Path
@@ -15,9 +14,15 @@ ROOT_DIR = Path(__file__).ancestor(1)
 @task
 def lint(ctx):
     # pylint: disable=W0613
-    """Run flake8 to lint code"""
-    run("python3 -m flake8")
+    """Run ruff and mypy to lint code"""
+    run("ruff check src/")
     run("python3 -m mypy src/")
+
+
+@task
+def outdated(ctx):
+    """List outdated packages"""
+    run("pip list --outdated")
 
 
 @task
@@ -30,25 +35,19 @@ def freeze(ctx):
 @task()
 def test(ctx):
     """Run unit tests"""
-    run("nose2")
+    run("nose2 -C")
 
 
 @task
 def release(ctx, deploy=False, test=False, version=""):
-    """Tag release, run Travis-CI, and deploy to PyPI"""
-    if test:
-        run("python3 setup.py check")
-        run("python3 setup.py register sdist upload --dry-run")
-
-    if deploy:
-        run("python3 setup.py check")
-        if version:
-            run("git checkout master")
-            run("git tag -a v{ver} -m 'v{ver}'".format(ver=version))
-            run("git push")
-            run("git push origin --tags")
-            run("python3 -m build")
-            run("python3 -m twine upload dist/*")
+    """Tag release and deploy to PyPI"""
+    if deploy and version:
+        run("git checkout master")
+        run("git tag -a v{ver} -m 'v{ver}'".format(ver=version))
+        run("git push")
+        run("git push origin --tags")
+        run("hatch build")
+        run("hatch publish")
     else:
         print("- Have you updated the version?")
         print("- Have you updated CHANGELOG.md?")
